c08924a81c7f7a0e2e75bbe2a5de3ea48ab3fb6a
Albert Moczadło <albertmoczadlo@gmail.com>
2022-11-21 09:01:38 +0100

    Replace added

---
+++ CulinaryRecipes/CulinaryRecipes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CulinaryRecipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
