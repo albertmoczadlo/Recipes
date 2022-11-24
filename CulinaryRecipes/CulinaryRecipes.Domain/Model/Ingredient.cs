21d7bb34c504b2d1c00c82b126b538ee49299b38
konserwa <grzegorzblaszczynski@gmail.com>
2022-11-24 19:44:24 +0100

    Początkowe tabele do listy składników

---
+++ CulinaryRecipes/CulinaryRecipes.Domain/Model/Ingredient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CulinaryRecipes.Domain.Model
+{
+
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public IngredientType IngradientType { get; set; }
+
+    }
+}
--- CulinaryRecipes/CulinaryRecipes.Infrastructure/Context.cs
+++ CulinaryRecipes/CulinaryRecipes.Infrastructure/Context.cs
@@ -6,14 +6,27 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using CulinaryRecipes.Domain.Model;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Reflection.Emit;
 
 namespace CulinaryRecipes.Infrastructure
 {
     public class appContext: IdentityDbContext
     {
+        public DbSet<Ingredient> Ingredient { get; set; }
+        public DbSet<IngredientInRecipe> IngredientInRecipe { get; set; }
+        public DbSet<Recipe> Recipe { get; set; }
+        public DbSet<IngredientType> IngredientType { get; set; }
+        public DbSet<IngredientTags> IngredientTags { get; set; }
+
         public appContext(DbContextOptions options):base(options)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
